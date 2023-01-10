dfe883499e5b8b3926d2407268931a98b7fd2509
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-10 15:29:22 -0500

    added delimiter formatting to txt

--- Ex_Files_Python_Excel/Exercise Files/fileRead.py
+++ Ex_Files_Python_Excel/Exercise Files/fileRead.py
@@ -3,6 +3,6 @@ from openpyxl.workbook import Workbook
 
 df_excel = pd.read_excel('regions.xlsx')
 df_csv = pd.read_csv('Names.csv')
-df_txt = pd.read_csv('data.txt')
+df_txt = pd.read_csv('data.txt', delimiter='\t')
 
-print(df_excel)
+print(df_txt)
