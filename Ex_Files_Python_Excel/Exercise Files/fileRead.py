a1c375ef2a0997467f16aa209957da3d66bfe8c5
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-10 15:22:46 -0500

    created venv for pandas and openpyxl

--- Ex_Files_Python_Excel/Exercise Files/fileRead.py
+++ Ex_Files_Python_Excel/Exercise Files/fileRead.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from openpyxl.workbook import Workbook
 
-df_excel = pd.read_excel("regions.xlsx")
-df_csv  = pd.read_csv("Names.csv")
-df_txt = pd.read_csv("data.txt")
+df_excel = pd.read_excel('regions.xlsx')
+df_csv = pd.read_csv('Names.csv')
+df_txt = pd.read_csv('data.txt')
+
+print(df_excel)
