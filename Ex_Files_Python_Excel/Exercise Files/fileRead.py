f5dc253308de4a0fd2e0f81fc7050fdef7cb862e
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-10 15:35:13 -0500

    created df csv columns

--- Ex_Files_Python_Excel/Exercise Files/fileRead.py
+++ Ex_Files_Python_Excel/Exercise Files/fileRead.py
@@ -2,7 +2,8 @@ import pandas as pd
 from openpyxl.workbook import Workbook
 
 df_excel = pd.read_excel('regions.xlsx')
-df_csv = pd.read_csv('Names.csv')
+df_csv = pd.read_csv('Names.csv', header=None)
 df_txt = pd.read_csv('data.txt', delimiter='\t')
 
-print(df_txt)
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Area Code']
+print(df_csv)
